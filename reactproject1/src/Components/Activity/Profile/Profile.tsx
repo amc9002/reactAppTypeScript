993980b781f8758a1b3302e285be531a15d32d56
amc1988 <9002amc@gmail.com>
2023-04-13 17:58:28 +0300

    wip

--- reactproject1/src/Components/Activity/Profile/Profile.tsx
+++ reactproject1/src/Components/Activity/Profile/Profile.tsx
@@ -8,6 +8,7 @@ const Profile = () => {
                 <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt=""></img>
             </div>
             <div>
+                <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt=""></img>
                 ava + descr
             </div>
         </div>
