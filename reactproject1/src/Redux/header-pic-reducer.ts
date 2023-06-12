64be5d2cdacd94ab9f0ba02b17413a1bfcb1d1f8
amc1988 <9002amc@gmail.com>
2023-06-13 00:56:22 +0300

    Rebuilt again

--- reactproject1/src/Redux/header-pic-reducer.ts
+++
@@ -1,5 +0,0 @@
-let initialState: string = 'https://img.freepik.com/free-vector/golden-bird-logo-design_1195-336.jpg?w=2000';
-
-const headerPicReducer = (state: any = initialState, action: any): string => state;
-
-export default headerPicReducer;
