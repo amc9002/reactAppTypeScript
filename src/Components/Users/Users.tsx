46289f4820f5a60047f95ba6a2aaec493975bf19
amc1988 <9002amc@gmail.com>
2023-08-09 23:33:00 +0300

    Class component 'Users' modified - componentDidMount method is defined

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -6,39 +6,40 @@ import { type PropsFromRedux } from './UsersContainer';
 import { UserType } from "../../types";
 
 class Users extends React.Component<PropsFromRedux> {
-    constructor(props: PropsFromRedux) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+
+    componentDidMount(): void {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
-    render() {
+    render(): JSX.Element {
         return (
             <div>
                 {this.props.users.map((u: UserType) =>
-                        <div key={u.id}>
+                    <div key={u.id}>
 
-                            <span>
-                                <div>
-                                    <img src={u.photos.small !== null ? u.photos.small : userPic} className={styles.ava} />
-                                </div>
-                                <div>
-                                    {u.followed
+                        <span>
+                            <div>
+                                <img src={u.photos.small !== null ? u.photos.small : userPic} className={styles.ava} />
+                            </div>
+                            <div>
+                                {u.followed
                                     ? <button onClick={() => { this.props.unFollowUser(u.id) }}>Unfollow</button>
                                     : <button onClick={() => { this.props.followUser(u.id) }}> Follow </button>}
-                                </div>
-                            </span>
-                            <span>
-                                <div>
-                                    {u.name}
-                                </div>
-                                <div>
-                                    {u.status}
-                                </div>
-                            </span>
-                        </div>
-                    )
+                            </div>
+                        </span>
+                        <span>
+                            <div>
+                                {u.name}
+                            </div>
+                            <div>
+                                {u.status}
+                            </div>
+                        </span>
+                    </div>
+                )
                 }
             </div>
         );
